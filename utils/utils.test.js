f22da26b4abf0ed6fa654e39b8bec80e398356c1
Weslley Nascimento Rocha <weslleytato.12@gmail.com>
2018-02-21 16:16:22 -0300

    initial commit

---
+++ utils/utils.test.js
@@ -0,0 +1,48 @@
+const expect = require('expect');
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+  var res = utils.add(33, 11);
+
+  expect(res)
+    .toBe(44)
+    .toBeA('number');
+
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}.`);
+  // }
+});
+
+it('should square a number', () => {
+  var res = utils.square(3);
+
+  expect(res)
+    .toBe(9)
+    .toBeA('number');
+
+  // if (res !== 9) {
+  //   throw new Error(`Expected 9, but got ${res}.`);
+  // }
+});
+
+it('should expect some values', () => {
+  // expect(12).toNotBe(12);
+  // expect({ nome: 'Weslley' }).toEqual({ nome: 'Weslley' });
+  // expect([2, 3, 4]).toInclude(3);
+  expect({
+    name: 'Weslley',
+    age: 21,
+    location: 'Brazil'
+  }).toInclude({ age: 21 });
+});
+
+it('should split fullname into user object', () => {
+  const user = utils.setName(
+    {
+      age: 21,
+      location: 'Brazil'
+    },
+    'Weslley Rocha'
+  );
+  expect(user).toInclude({ firstName: 'Weslley', lastName: 'Rocha' });
+});
